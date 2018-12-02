0911318f3b4a7c6f765a9196206ca2eb2dd476b7
r-sreesaran <r.sreesaran@gmail.com>
2018-12-02 15:09:34 +0530

    added solution to LittleElephantAndCandies

--- src/main/java/arrays/LittleElephantAndCandies.java
+++ src/main/java/arrays/LittleElephantAndCandies.java
@@ -1,5 +1,39 @@
 package main.java.arrays;
 
-public class LittleElephantAndCandies {
+import java.math.BigInteger;
 
+class LittleElephantAndCandies {
+
+     static void isItpossible(String params[],String candiesSplit[]) {
+
+        BigInteger candy = BigInteger.valueOf(Long.getLong(params[0]));
+        BigInteger elephants = BigInteger.valueOf(Long.getLong(params[1]));
+        BigInteger totalCandies=BigInteger.valueOf(0);
+        if(candy.compareTo(elephants)<=0) {
+            System.out.println("No");
+            return;
+        }
+
+        for(String value: candiesSplit) {
+            totalCandies = totalCandies.add(BigInteger.valueOf(Long.valueOf(value))) ;
+        }
+        if(totalCandies.compareTo(elephants)>=0) {
+            System.out.println("No");
+            return;
+        }
+        System.out.println("Yes");
+    }
+
+    public static void main(String args[]) {
+        BigInteger totalNoOfTestcases=BigInteger.valueOf(Long.valueOf(args[0]));
+        String params[] = args[1].split("");
+        String candiesSplit[] = args[2].split("");
+
+        if(BigInteger.valueOf(candiesSplit.length).compareTo(BigInteger.valueOf(Long.valueOf(params[1])))<0) {
+            System.out.println("No");
+            return;
+        }
+        isItpossible(params,candiesSplit);
+
+    }
 }
