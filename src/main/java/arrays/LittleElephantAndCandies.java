9dfadc55722808a54584da95d12e713fc84a7ab6
r-sreesaran <r.sreesaran@gmail.com>
2018-12-02 15:11:37 +0530

    refractored solution

--- src/main/java/arrays/LittleElephantAndCandies.java
+++ src/main/java/arrays/LittleElephantAndCandies.java
@@ -1,10 +1,12 @@
+package main.java.arrays;
+
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 import java.math.BigInteger;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Codechef
+class LittleElephantAndCandies
 {
     public static void main (String[] args) throws java.lang.Exception
     {
