09a881f680d27c054899414f2c8c066a0513e54a
r-sreesaran <r.sreesaran@gmail.com>
2019-02-21 10:05:18 +0530

    added stpar

--- pythonSolution/streetParade.py
+++ pythonSolution/streetParade.py
@@ -1,57 +1,30 @@
-'''
-10
 
-5
-3 1 2 5 4
-5
-3 1 2 5 4
-0
-
-
-
-no
-5
-1 3 4 2 5
-no
-4
-4 2 3 1
-no
-1 2 10 5 4 3 9 8 7 6
-yes
-4 1 5 3 2
-no
-5
-3 1 2 5 4
-yes
-5
-5 1 2 4 3
-0
-'''
-
-lane=[];
+st=[];
 state=True
-need = 1
 
 while True:
  size = int(input())
  if(size!=0):
-    order = list(map(int,input().split(" ")))
-    for i in range(size):
-        while not (not lane) and lane[-1] == need:
-            need+=1
-            lane.pop()
-        if(order[i]==need):
-            need=+1
-        elif not (not lane) and lane[-1]<order[i]:
-            state = False
-            break
+    a = list(map(int,input().split(" ")))
+    i = 0
+    k=0
+
+    while(i<size):
+        while not (not st) and st[-1] == k+1:
+            k=k+1
+            st.pop()
+        if(a[i]!=k+1):
+            st.append(a[i])
         else:
-            lane.append(order[i])
-    if(state):
-       print ("yes")
+            k=k+1
+        i=i+1
+    while not (not st) and st[-1] == k+1:
+        k = k +1
+        st.pop()
+    if(k==size):
+        print("yes")
     else:
-       print ("no")
+        print("no")
  else:
     break
-
 
