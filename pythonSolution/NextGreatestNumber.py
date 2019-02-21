137fb2b610b4c2838388553ca292399c316f3f08
r-sreesaran <r.sreesaran@gmail.com>
2019-02-22 00:07:13 +0530

    added solution for jNext

--- pythonSolution/NextGreatestNumber.py
+++ pythonSolution/NextGreatestNumber.py
@@ -2,5 +2,25 @@ tc = int(input())
 for i in range(0,tc):
  len = int(input())
  number = list(map(int,input().split()))
+ i=0
+ exchange = True
+ for i in range(len-1,0,-1):
+  if(number[i]>number[i-1]):
+   number[i],number[i-1] = number[i-1],number[i]
+   exchange = False
+   break;
+
+  if(exchange):
+   print("no")
+  else:
+    newlist = number(0,i)
+    newlist.append(number(i,len-1).sort())
+    print(newlist)
+
+
+
+
+
+
 
  
