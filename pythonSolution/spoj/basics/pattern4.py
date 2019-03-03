dd7f9411034ca2db7ef7611bc535c1effa6200bb
r-sreesaran <r.sreesaran@gmail.com>
2019-03-03 23:22:11 +0530

    added pattern 4

---
+++ pythonSolution/spoj/basics/pattern4.py
@@ -0,0 +1,12 @@
+#https://www.spoj.com/problems/CPTTRN4/
+#3 1 2 1
+t=int(input())
+for i in range(t):
+    r, c, h, w = map(int, input().split())
+    for k in range(r*3):
+        for j in range(c*3):
+
+
+        print()
+    print()
+
