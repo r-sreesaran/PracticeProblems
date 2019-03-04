21b42e5a7ee8794006780cc49439d053fcf28b9c
r-sreesaran <r.sreesaran@gmail.com>
2019-03-04 16:40:54 +0530

    added solution to pattern 4

--- pythonSolution/spoj/basics/pattern4.py
+++ pythonSolution/spoj/basics/pattern4.py
@@ -1,12 +1,15 @@
 #https://www.spoj.com/problems/CPTTRN4/
-#3 1 2 1
 t=int(input())
 for i in range(t):
     r, c, h, w = map(int, input().split())
-    for k in range(r*3):
-        for j in range(c*3):
-
-
+    h = h + 1
+    w= w+1
+    for k in range(r*h+1):
+        for j in range(c*w+1):
+            if(k%h==0 or j%w==0):
+                print("*",end="")
+            else:
+                print(".",end="")
         print()
     print()
 
