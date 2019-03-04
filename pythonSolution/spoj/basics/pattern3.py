2e6d50e18aaea784f365d5ccdb280ff1ee76cc4a
r-sreesaran <r.sreesaran@gmail.com>
2019-03-04 16:43:49 +0530

    removed unnecessary conidtion

--- pythonSolution/spoj/basics/pattern3.py
+++ pythonSolution/spoj/basics/pattern3.py
@@ -4,7 +4,7 @@ for i in range(t):
     r, c = map(int, input().split())
     for k in range(r*3+1):
         for j in range(c*3+1):
-            if(k==0 or j==0 or j%3==0 or k%3==0  ):
+            if( j%3==0 or k%3==0  ):
                 print("*",end="")
             else:
                 print(".",end="")
