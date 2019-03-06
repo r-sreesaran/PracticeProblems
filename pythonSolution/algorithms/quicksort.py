869caa4740a469b458d7bc24ff69da77313792b1
r-sreesaran <r.sreesaran@gmail.com>
2019-03-06 09:56:02 +0530

    added solution to quick sort

--- pythonSolution/algorithms/quicksort.py
+++ pythonSolution/algorithms/quicksort.py
@@ -8,13 +8,32 @@ def quickSort(alist):
 def quickSortHelper(alist,first,last):
      if first<last:
          splitpoint = partition(alist,first,last)
+         quickSortHelper(alist,first,splitpoint-1)
+         quickSortHelper(alist,splitpoint+1,last)
 
-     quickSortHelper(alist,first,splitpoint+1)
-     quickSortHelper(alist,splitpoint,last-1)
+def partition(alist,first,last):
+    partitionValue = alist[first]
 
-def partition():
+    done=False
+    leftmark = first + 1
+    rightmark = last
 
+    while not done:
+        while leftmark<=rightmark and alist[leftmark]<=partitionValue:
+            leftmark = leftmark +1
+        while rightmark>=leftmark and alist[rightmark]>=partitionValue:
+            rightmark=rightmark-1
+        if leftmark>rightmark:
+            done=True
+        else:
+          temp = alist[rightmark]
+          alist[rightmark]=alist[leftmark]
+          alist[leftmark]=temp
 
+    temp = alist[first]
+    alist[first]=alist[rightmark]
+    alist[rightmark]=temp
+    return rightmark
 
 alist = [54, 26, 78, 17, 77, 48, 55, 31, 20]
 quickSort(alist)
