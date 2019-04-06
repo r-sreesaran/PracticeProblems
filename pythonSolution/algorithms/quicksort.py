2b50b81808730addde64a1f333ec22ed867a9bba
r-sreesaran <r.sreesaran@gmail.com>
2019-04-06 22:37:40 +0530

    added return types

--- pythonSolution/algorithms/quicksort.py
+++ pythonSolution/algorithms/quicksort.py
@@ -5,13 +5,13 @@ def quickSort(alist):
     return quickSortHelper(alist, 0, len(alist) - 1)
 
 
-def quickSortHelper(alist,first,last):
+def quickSortHelper(alist,first: int,last: int) -> None:
      if first<last:
          splitpoint = partition(alist,first,last)
          quickSortHelper(alist,first,splitpoint-1)
          quickSortHelper(alist,splitpoint+1,last)
 
-def partition(alist,first,last):
+def partition(alist,first: int,last: int) -> None:
     partitionValue = alist[first]
 
     done=False
