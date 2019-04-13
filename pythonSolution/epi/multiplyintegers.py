d95f4b7d2c0d64e65f5931690a015f3e76f9114e
r-sreesaran <r.sreesaran@gmail.com>
2019-04-13 18:33:50 +0530

    added solution

--- pythonSolution/epi/multiplyintegers.py
+++ pythonSolution/epi/multiplyintegers.py
@@ -1,13 +1,21 @@
-no1=[1,2,3] 
-no2=[3,2,1]
+from typing import List
+
+no1=[1,9,3,7,0,7,7,2,1] 
+no2=[7,6,1,8,3,8,2,5,7,2,8,7]
 result=[]
 carryover=0
+def multiply(num1: List[int], num2: List[int]):
+    sign = -1 if(num1[0]<0) ^ (num2[0]<0) else 1
 
-for i in reversed(range(len(no1)-1)):
-    ans = no1[i] * no2[i]
-    if(ans<10):
-       result.append(ans+carryover)
-    else:
-        result.append(ans%10)
-        carryover = int(ans/10)
+    result = [0] * (len(num1) + len(num2))
+    for i in reversed(range(len(num1))):
+        for j in reversed(range(len(num2))):
+            result[i+j+1] += num1[i] * num2[j]
+            result[i+j] += result[i+j+1] // 10
+            result[i+j+1] %= 10
+    result = result[next((i for i, x in enumerate(result)
+    if x !=0 ), len(result)): ] or [0]
+    return [sign * result[0]] + result[1:]
+result= multiply(no1,no2)
 print(result)
+        
