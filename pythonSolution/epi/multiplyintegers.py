a787be6eda0a7164ce0ce6a46927ce48d40d0966
r-sreesaran <r.sreesaran@gmail.com>
2019-04-14 23:44:57 +0530

    remove leading zeros

--- pythonSolution/epi/multiplyintegers.py
+++ pythonSolution/epi/multiplyintegers.py
@@ -1,20 +1,21 @@
 from typing import List
 
-no1=[1,9,3,7,0,7,7,2,1] 
-no2=[7,6,1,8,3,8,2,5,7,2,8,7]
+no1=[3,3,3]
+no2=[3,3,3,0]
 result=[]
 carryover=0
 def multiply(num1: List[int], num2: List[int]):
-    sign = -1 if(num1[0]<0) ^ (num2[0]<0) else 1
-
+    sign = -1 if(num1[0]<0) or (num2[0]<0) else 1
+    num1[0],num2[0]=abs(num1[0]),abs(num2[0])
     result = [0] * (len(num1) + len(num2))
     for i in reversed(range(len(num1))):
         for j in reversed(range(len(num2))):
             result[i+j+1] += num1[i] * num2[j]
             result[i+j] += result[i+j+1] // 10
             result[i+j+1] %= 10
-    result = result[next((i for i, x in enumerate(result)
-    if x !=0 ), len(result)): ] or [0]
+
+    for i,x in enumerate(result): print(i,x)
+    result = result[next((i for i, x in enumerate(result) if x !=0 )):]
     return [sign * result[0]] + result[1:]
 result= multiply(no1,no2)
 print(result)
