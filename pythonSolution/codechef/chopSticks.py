eda59012b219b2d3be6dd68af0ed0b185e474e0c
r-sreesaran <r.sreesaran@gmail.com>
2019-04-16 00:50:36 +0530

    decreased the running time of the problem

--- pythonSolution/codechef/chopSticks.py
+++ pythonSolution/codechef/chopSticks.py
@@ -4,15 +4,15 @@ numbers=[0]*n
 arb=[0]*n
 for i in range(n):
     numbers[i]=int(input())
-    if(i>0):
-        if(abs(numbers[i-1]-numbers[i])<=d and arb[i]!=1 and arb[i-1]!=1):
-            arb[i]=arb[i-1]=1
 
-for i in range(n):
-    for j in range(i,n):
-        if(arb[i]!=1 and arb[j]!=1 and abs(numbers[i]-numbers[j])<=d):
-            arb[i] = arb[j] = 1
+numbers.sort()
 
+i=0
+j=1
+for k in range(n-1):
+        if(arb[k]!=1 and arb[k+1]!=1 and abs(numbers[k]-numbers[k+1])<=d):
+               arb[k] = arb[k+1] = 1
+        
 
-print(arb.count(1)/2)
+print(arb.count(1)//2)
 
