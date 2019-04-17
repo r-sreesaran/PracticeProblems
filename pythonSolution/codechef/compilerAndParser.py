fe69996f044ba9954e3ba60d3336718103d64170
r-sreesaran <r.sreesaran@gmail.com>
2019-04-18 00:30:00 +0530

    added solution to compiler problem

--- pythonSolution/codechef/compilerAndParser.py
+++ pythonSolution/codechef/compilerAndParser.py
@@ -1,3 +1,17 @@
 #https://www.codechef.com/problems/COMPILER
-letters = list(input.split(""))
-print(letters)
+n = int(input())
+
+for _ in range(n):
+    letters = list(input())
+    pair=0
+    open=0
+
+    for i in letters:
+        if( i is '>' and open >=1):
+          open-=1
+          pair+=1
+        elif(i is '<'):
+           open+=1
+    print(pair*2)
+
+    
