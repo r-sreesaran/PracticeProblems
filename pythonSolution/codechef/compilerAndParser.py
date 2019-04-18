5d279c6230bfd1026acd702a9275b0b57afc6f7a
r-sreesaran <r.sreesaran@gmail.com>
2019-04-18 14:37:14 +0530

    added solution to compiler problem

--- pythonSolution/codechef/compilerAndParser.py
+++ pythonSolution/codechef/compilerAndParser.py
@@ -5,13 +5,19 @@ for _ in range(n):
     letters = list(input())
     pair=0
     open=0
+    close=0
 
-    for i in letters:
-        if( i is '>' and open >=1):
-          open-=1
-          pair+=1
-        elif(i is '<'):
+    for i in range(len(letters)):
+        if( letters[i] is '>'):
+          close+=1
+        elif(letters[i] is '<'):
            open+=1
-    print(pair*2)
+        if(open-close==0):
+            pair=i+1
+        elif(close>open):
+            print(0)
+            break;    
+
+    print(temp)
 
     
