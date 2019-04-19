f83ed50f1b0ef8f9e5d7a9de5069bee9ebbbb741
r-sreesaran <r.sreesaran@gmail.com>
2019-04-19 17:45:05 +0530

    added solution to stpar

--- pythonSolution/spoj/stpar.py
+++ pythonSolution/spoj/stpar.py
@@ -1,1 +1,26 @@
 #https://www.spoj.com/problems/STPAR/
+
+
+while(True):
+ n = int(input())
+ if(n==0):
+    break
+ sequence=list(map(int,input().split()))
+ k=1
+ orderedList=[0]*n
+ for i in range(n):
+    if(k==sequence[i]):
+        orderedList[k-1]=sequence[i]
+        k+=1
+ 
+ for j in reversed(range(n)):
+    if(k==sequence[j]):
+        orderedList[k-1]=sequence[j]
+        k+=1
+ if(k-1==n):
+    print("yes")
+ else:
+    print("no")
+
+
+
