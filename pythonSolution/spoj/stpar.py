0b5958a50b86a342068849930f07846b57262874
r-sreesaran <r.sreesaran@gmail.com>
2019-04-20 02:19:27 +0530

    added solution to stpar

--- pythonSolution/spoj/stpar.py
+++ pythonSolution/spoj/stpar.py
@@ -6,21 +6,23 @@ while(True):
  if(n==0):
     break
  sequence=list(map(int,input().split()))
+ status=True
  k=1
- orderedList=[0]*n
- for i in range(n):
-    if(k==sequence[i]):
-        orderedList[k-1]=sequence[i]
-        k+=1
- 
- for j in reversed(range(n)):
-    if(k==sequence[j]):
-        orderedList[k-1]=sequence[j]
-        k+=1
- if(k-1==n):
-    print("yes")
- else:
-    print("no")
+ tempList=[]
+ for e in sequence:
+     while bool(len(tempList)) and tempList[-1]==k:
+         k=k+1
+         tempList.pop()
 
+     if(e==k):
+         k=k+1
 
+     elif bool(len(tempList)) and e>tempList[-1]:
+         print("no")
+         status=False
+         break
+     else:
+         tempList.append(e)
 
+ if status:
+  print("yes")
