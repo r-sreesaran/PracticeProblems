ed9eefcb44005515ebf38ae555899836d4b552fa
r-sreesaran <r.sreesaran@gmail.com>
2019-04-24 00:20:34 +0530

    added solution to jnext

--- pythonSolution/spoj/jnext.py
+++ pythonSolution/spoj/jnext.py
@@ -1,28 +1,32 @@
 #https://www.spoj.com/problems/JNEXT/
 t = int(input())
-diff = 1 
-found = False
 
 for k in range(t):
  n = int(input())
  no = list(map(int,input().split(" ")))
  i=n-1
- while((not found) and diff<n):
-     if(i-diff>0):
-        if(no[i-diff]>no[i]):
+ diff = 1
+ found = False
+ while((not found) and i>0):
+     if(i-diff>0 and diff<n):
+        if(no[i-diff]<no[i]):
             no[i],no[i-diff]=no[i-diff],no[i]
             found=True
             break
         if(i==1):
             diff=diff+1
             i=n-1
- i=i-1
+     i=i-1
 
  if(found==True):
-    list1 = no[0:i+1]
-    list2 = no[i+1:n]
+    list1 = no[0:i]
+    list2 = no[i:n]
     list2.sort()
     no=list1+list2
+    no=''.join(str(x) for x in no)
     print(no)
-else:
+
+ else:
     print(-1)
+
+
