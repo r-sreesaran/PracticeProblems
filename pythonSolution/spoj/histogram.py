06318b089a886541726524b22d2f3ea4aeeee996
r-sreesaran <r.sreesaran@gmail.com>
2019-05-10 01:50:39 +0530

    added solution for histrogram pronlem

--- pythonSolution/spoj/histogram.py
+++ pythonSolution/spoj/histogram.py
@@ -1,18 +1,20 @@
 #https://www.spoj.com/problems/HISTOGRA/
 
-n=-1
-while(n!=0):
-    n=list(map(int,input().split(" ")))
-    if(n!=0):
+n=[-1]
+while(n[0]!=0):
+    n=list(map(int,input().split()))
+    a=0
+    if(n[0]!=0):
       length = n[0]
       i=1
-      max=0
-      while(i<=length-1):
+      m=0
+      while(i<=length):
            j=1
-           while(j+i<n):
-              l=max(n[j:i+j+1])
-              a = l*i
-              if(a>max):
-                   a=max
+           while(j+i<=length):
+              l=min(n[j:i+j+1])
+              a = l*(i+1)
+              if(m<a):
+                   m=a
               j+=i
-        i+=1
+           i+=1
+      print(m)
