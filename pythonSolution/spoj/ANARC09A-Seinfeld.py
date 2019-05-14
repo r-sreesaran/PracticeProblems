5d28845872b9b0000f65471662185b7a1b3bdb6c
r-sreesaran <r.sreesaran@gmail.com>
2019-05-14 10:03:27 +0530

    added solution to seinfeild

--- pythonSolution/spoj/ANARC09A-Seinfeld.py
+++ pythonSolution/spoj/ANARC09A-Seinfeld.py
@@ -1,35 +1,9 @@
-#https://www.spoj.com/problems/ANARC09A/
-no=1
-while(True):
-    n=list(input().strip())
-    stack=[]
-    if(n[0]!='-'):
-        for e in n:
-            if(e=='{'):
-                stack.append(e)
-            elif(e=='}' and stack):
-                if(stack[-1]=='{'):
-                 stack.pop()
-                else:
-                    stack.append('}')
-            elif(e=='}' and not stack):
-                stack.append('}')
-        b=0
-        pair = False
-        for i in range(0,len(stack)-1,2):
-            if(stack): 
-              if(stack[i]=='{' and stack[i+1]=='{'):
-                b+=1
-              elif(stack[i]=='}' and stack[i+1]=='}'):
-                 b+=1
-              else:
-                   pair=True  
-
-        if(pair==True):
-          b=+2
-        if(len(stack)%2!=0):
-           b+=1
-        print(str(no)+'. '+ str(b))
-        no+=1
-    else:
-        break
+import sys
+for cs, s in enumerate(sys.stdin.readlines(), 1):
+	if s[0] == '-': break
+	n, cnt = 0, 0
+	for c in s:
+		if c == '{': n+= 1
+		elif c == '}' and n==0: n+= 1; cnt+= 1
+		elif c == '}' and n>0: n-= 1
+	print('%d. %d' % (cs, n/2+cnt))
