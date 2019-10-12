206efe5ccae94f3cb4bfddf160e27a0180f58b07
r-sreesaran <r.sreesaran@gmail.com>
2019-10-12 18:27:04 +0530

    added solution

--- pythonSolution/codechef/sample.py
+++ pythonSolution/codechef/sample.py
@@ -15,6 +15,8 @@ for i in string:
     else:
         firstHalf = int(length / 2)
         secondHalf = firstHalf + 1
-    string1 = ''.join(list(i)[0:firstHalf])
-    string2 = ''.join(list(i)[secondHalf:length+1])
-    print(('NO','YES') [string1 in string2])
+    string1 = list(i)[0:firstHalf]
+    string2 = list(i)[secondHalf:length+1]
+    string1.sort()
+    string2.sort()
+    print(('NO','YES') [string1 == string2])
