0e24246ae5426dabf315f39c196f8578a64a53fc
r-sreesaran <r.sreesaran@gmail.com>
2019-11-30 18:12:27 +0530

    added corrected solution

--- pythonSolution/excercism/python/pangram/pangram.py
+++ pythonSolution/excercism/python/pangram/pangram.py
@@ -1,5 +1,17 @@
+import re
+
 def is_pangram(sentence):
-    sentence= sentence.replace(' ','')
+    sentence = re.sub('[^A-Za-z]+', '', sentence)
+    sentence= sentence.replace(' ','').lower()
     splittedChar =  list(sentence)
     uniqueChar = set(splittedChar)
-    return len(uniqueChar) >=  35
+    return len(uniqueChar) >=  26
+
+
+def is_pangram(sentence):
+    sent = sentence.lower()
+    alphabets = "abcdefghijklmnopqrstuvwxyz"
+    for letter in alphabets:
+        if letter not in sent:
+            return False
+    return True
