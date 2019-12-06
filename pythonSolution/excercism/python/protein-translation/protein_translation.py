4e4fee9ef1f4243eecc0ee5e7854dc4ed1ba7271
r-sreesaran <r.sreesaran@gmail.com>
2019-12-06 07:40:14 +0530

    started with protein translation

---
+++ pythonSolution/excercism/python/protein-translation/protein_translation.py
@@ -0,0 +1,9 @@
+
+
+
+def proteins(strand):
+    proteinMap = {"Methionine":"AUG","Phenylalanine":["UUU","UUC"],"Leucine":["UUA","UUG"],
+                  "Serine":["UCU", "UCC", "UCA","UCG"],"Tyrosine":["UAU","UAC"],
+                  "Cysteine":["UGC","UGU"], "Tryptophan":"UGG", "STOP":["UAA", "UAG", "UGA"]
+                  }
+    pass
