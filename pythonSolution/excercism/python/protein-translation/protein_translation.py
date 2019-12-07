25a377c9f5d0e949c67be2911ef3e62cac6be9c4
r-sreesaran <r.sreesaran@gmail.com>
2019-12-07 11:27:56 +0530

    added solution for rain drops

--- pythonSolution/excercism/python/protein-translation/protein_translation.py
+++ pythonSolution/excercism/python/protein-translation/protein_translation.py
@@ -1,9 +1,23 @@
-
+from textwrap import wrap
 
 
 def proteins(strand):
-    proteinMap = {"Methionine":"AUG","Phenylalanine":["UUU","UUC"],"Leucine":["UUA","UUG"],
-                  "Serine":["UCU", "UCC", "UCA","UCG"],"Tyrosine":["UAU","UAC"],
-                  "Cysteine":["UGC","UGU"], "Tryptophan":"UGG", "STOP":["UAA", "UAG", "UGA"]
+    proteinMap = {"Methionine": "AUG", "Phenylalanine": ["UUU", "UUC"], "Leucine": ["UUA", "UUG"],
+                  "Serine": ["UCU", "UCC", "UCA", "UCG"], "Tyrosine": ["UAU", "UAC"],
+                  "Cysteine": ["UGC", "UGU"], "Tryptophan": "UGG", "STOP": ["UAA", "UAG", "UGA"]
                   }
-    pass
+    items = wrap(strand, 3)
+    proteinName = []
+    for el in items:
+        for protein, acronym in proteinMap.items():
+
+            if type(acronym) is str:
+                if acronym == el:
+                    proteinName.append(protein)
+
+            if type(acronym) is list:
+                 if el in acronym:
+                      if(protein=='STOP'): return proteinName
+                      proteinName.append(protein)
+
+    return proteinName
