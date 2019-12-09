4982a6b44016503427bd781ab3092b472a098294
r-sreesaran <r.sreesaran@gmail.com>
2019-12-09 23:57:26 +0530

    added solution to two programs

--- pythonSolution/excercism/python/high-scores/high_scores.py
+++ pythonSolution/excercism/python/high-scores/high_scores.py
@@ -1,10 +1,15 @@
 def latest(scores):
+    return scores[len(scores)-1]
     pass
 
 
 def personal_best(scores):
-    pass
+    scores.sort(reverse = True)
+    return scores[0]
+
 
 
 def personal_top_three(scores):
-    pass
+    scores.sort(reverse = True)
+    return scores[:3]
+
