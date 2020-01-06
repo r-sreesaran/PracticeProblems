4dfaf4133d5eedc961b3e059a18f0a17d3228d29
r-sreesaran <r.sreesaran@gmail.com>
2020-01-06 23:43:38 +0530

    added solutions for find the permutations of a string in o(n) run time

---
+++ pythonSolution/algorithms/permutations.py
@@ -0,0 +1,11 @@
+
+def permutations(original,prefix):
+    if(not original):
+        print(prefix)
+    else:
+        for i in range(len(original)):
+            rem = original[0:i]+original[i+1:]
+            permutations(rem,prefix+original[i])
+
+
+permutations("abc","")
