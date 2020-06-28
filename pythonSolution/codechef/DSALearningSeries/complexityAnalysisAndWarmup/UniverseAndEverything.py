e8348643ddc842276a7b373971473ab1105b9d40
r-sreesaran <r.sreesaran@gmail.com>
2020-06-28 10:30:14 +0530

    added solution

---
+++ pythonSolution/codechef/DSALearningSeries/complexityAnalysisAndWarmup/UniverseAndEverything.py
@@ -0,0 +1,12 @@
+#https://www.codechef.com/LRNDSA01/problems/TEST
+try:
+   while (True):
+    i=int(input())
+    if(i!=42):
+         print(i)
+    else:
+         break
+except Exception :
+    pass
+
+
