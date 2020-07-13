092b298982afedaf664dc5c0146ca22a2a91bfc1
r-sreesaran <r.sreesaran@gmail.com>
2020-07-13 23:27:10 +0530

    added method to print the no of zeros

--- pythonSolution/codechef/DSALearningSeries/complexityAnalysisAndWarmup/Factorial.py
+++ pythonSolution/codechef/DSALearningSeries/complexityAnalysisAndWarmup/Factorial.py
@@ -10,6 +10,14 @@ def fact(n, start=2):
     for i in range(start, n + 1):
         fact_values.append(fact_values[i-1] * i)
 
+def divisibility(temp,no):
+    if(no%10==0):
+        temp = temp+1
+        temp = divisibility(temp, int(no / 10))
+    else:
+        return temp
+
+    return  temp
 
 for _ in range(a):
     n = int(input())
@@ -17,4 +25,4 @@ for _ in range(a):
         value = fact_values[n]
     except IndexError:
        fact(n,len(fact_values))
-    print(fact_values[n])
+    print(divisibility(0,fact_values[n]))
