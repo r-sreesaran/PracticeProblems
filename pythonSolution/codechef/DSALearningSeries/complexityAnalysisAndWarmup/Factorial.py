425d240010ad077312ab65ac84037191e32822da
r-sreesaran <r.sreesaran@gmail.com>
2020-07-14 20:18:05 +0530

    added solution to factorial problem

--- pythonSolution/codechef/DSALearningSeries/complexityAnalysisAndWarmup/Factorial.py
+++ pythonSolution/codechef/DSALearningSeries/complexityAnalysisAndWarmup/Factorial.py
@@ -1,28 +1,51 @@
 # factorial using memorization
+# tip https://brilliant.org/wiki/trailing-number-of-zeros/
+
 a = int(input())
-fact_values = []
-divisble = []
-fact_values.append(1)
-fact_values.append(1)
 
-def fact(n, start=2):
-
-    for i in range(start, n + 1):
-        fact_values.append(fact_values[i-1] * i)
-
-def divisibility(temp,no):
-    if(no%10==0):
-        temp = temp+1
-        temp = divisibility(temp, int(no / 10))
-    else:
-        return temp
-
-    return  temp
+def find_no_of_zeros(n):
+   count=0;
+   intial = 5;
+   if(n<5):
+       return 0;
+   while (intial < n):
+      count =  count + int(n/intial)
+      intial= intial * 5
+   return count
 
 for _ in range(a):
     n = int(input())
-    try:
-        value = fact_values[n]
-    except IndexError:
-       fact(n,len(fact_values))
-    print(divisibility(0,fact_values[n]))
+    print(find_no_of_zeros(n))
+
+
+
+#fact_values.append(1)
+#  fact_values.append(1)
+
+# def fact(n, start=2):
+#
+#     for i in range(start, n + 1):
+#         fact_values.append(fact_values[i-1] * i)
+
+#Traceback (most recent call last):
+#  File "./prog.py", line 29, in <module>
+#  File "./prog.py", line 17, in divisibility
+#  File "./prog.py", line 17, in divisibility
+#  File "./prog.py", line 17, in divisibility
+#  [Previous line repeated 994 more times]
+#  File "./prog.py", line 15, in divisibility
+# RecursionError: maximum recursion depth exceeded in comparison
+
+
+# def divisibility(temp,no):
+#     if(no%10==0):
+#         temp = temp+1
+#         temp = divisibility(temp, int(int(no) // int(10)))
+#     else:
+#         return temp
+#
+#     return  temp
+
+# def divisibility(no):
+#     str(no)
+
