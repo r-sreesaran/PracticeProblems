bc590a3cf700b8998311275e3980a55c94b724e7
r-sreesaran <r.sreesaran@gmail.com>
2020-09-27 07:59:21 +0530

    added solution

--- Solutions/codechef/DSALearningSeries/complexityAnalysisAndWarmup/coin_flip.py
+++ Solutions/codechef/DSALearningSeries/complexityAnalysisAndWarmup/coin_flip.py
@@ -1,1 +1,4 @@
 #https://www.codechef.com/LRNDSA01/problems/CONFLIP
+# The no of test cases
+
+
--- Solutions/excercism/python/matrix/matrix.py
+++ Solutions/excercism/python/matrix/matrix.py
@@ -1,22 +1,21 @@
 class Matrix:
     def __init__(self, matrix_string):
         self.matrix_string = matrix_string
+        self.process_matrix_string()
 
     def row(self, index):
-        matrix_row = self.process_matrix_string()
-        return matrix_row[index-1]
+
+        return self.matrix_row[index-1]
 
     def column(self, index):
-        matrix_row  = self.process_matrix_string()
         column = []
-        for row in matrix_row:
+        for row in self.matrix_row:
             column.append(row[index-1])
         return column
 
-    def process_matrix_string(self) :
-        matrix_row = []
+    def _process_matrix_string(self) :
+        self.matrix_row = []
         rows = self.matrix_string.splitlines()
         for row in rows :
             new_row  = [int(x) for x in row.split()]
-            matrix_row.append(new_row)
-        return matrix_row 
+            self.matrix_row.append(new_row)
