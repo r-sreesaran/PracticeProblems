de0e1003b29d22e89458a2776195b6d5e89c81e2
r-sreesaran <r.sreesaran@gmail.com>
2020-10-08 19:06:58 +0530

    added solution

--- Solutions/codechef/DSALearningSeries/complexityAnalysisAndWarmup/Laddu.py
+++ Solutions/codechef/DSALearningSeries/complexityAnalysisAndWarmup/Laddu.py
@@ -1,35 +1,57 @@
 #https://www.codechef.com/LRNDSA01/problems/LADDU
-
-import enum
-
-class Point(enum.Enum):
-   CONTEST_WON = 300
-   TOP_CONTRIBUTOR = 300
-   CONTEST_HOSTED = 50
-   INDIAN = 200
-   NON_INDIAN = 400
+#
+# import enum
+#
+# class Point(enum.Enum):
+#    CONTEST_WON = 300
+#    TOP_CONTRIBUTOR = 300
+#    CONTEST_HOSTED = 50
+#    INDIAN = 200
+#    NON_INDIAN = 400
+#    BUG_FOUND = 0
+#
+# activities = int(input())
+# for t in range(activities):
+#     total = 0
+#     n,nationality = map(str,input().split(" "))
+#     n = int(n)
+#
+#     for item in range(n):
+#         l1 = list(map(str,input().split(" ")))
+#         if len(l1)>0:
+#
+#             if l1[0]==Point.CONTEST_WON.name :
+#                 no = int(l1[1])
+#                 total += Point.CONTEST_WON.value + (20 -no if  no <=20 else 0)
+#             if l1[0] ==Point.CONTEST_HOSTED.name :
+#                 total += Point.CONTEST_HOSTED.value
+#             if l1[0] == Point.TOP_CONTRIBUTOR.name:
+#                 total += Point.TOP_CONTRIBUTOR.value
+#             if l1[0] == Point.BUG_FOUND.name:
+#                 total += int(l1[1])
+#
+#     print(int(total/Point[nationality].value))
 
 activities = int(input())
 for t in range(activities):
     total = 0
-    n,nationality = map(str,input().split(""))
+    n,nationality = map(str,input().split(" "))
+    n = int(n)
     total = 0
 
     for item in range(n):
         no = 0;
-        print(item)
-        l1 = map(str,input().split(" "))
-        if len(l1)>1:
-            no = int(l1[1])
-            if l1[0]=='CONTEST_WON' and no<20 :
-                total = total + 20 - no
+        l1 = list(map(str,input().split(" ")))
+        if len(l1)>0:
 
+            if l1[0]=="CONTEST_WON" :
+                no = int(l1[1])
+                total += 300 + (20 -no if  no <=20 else 0)
+            if l1[0] =="CONTEST_HOSTED" :
+                total += 50
+            if l1[0] == "TOP_CONTRIBUTOR":
+                total += 300
+            if l1[0] == "BUG_FOUND":
+                total += int(l1[1])
 
-        total = total + Point.l1[0] + no[0]
-    print(int(total/Point.nationality))
-
-
-
-
-
-
+    print(total// (200 if nationality == "INDIAN" else 400))
