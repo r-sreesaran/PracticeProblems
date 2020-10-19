aea2f9c92fa6f4acf4d3641586eaa541b27af19a
r-sreesaran <r.sreesaran@gmail.com>
2020-10-19 20:54:50 +0530

    added solution

--- Solutions/codechef/DSALearningSeries/LinearDataStructures/matchedbrackets.py
+++ Solutions/codechef/DSALearningSeries/LinearDataStructures/matchedbrackets.py
@@ -17,7 +17,8 @@ for i in range(l):
         depth +=1
         if(depth>max_depth):
             max_depth = depth
-            max_depth_pos = i-1
+            max_depth_pos = i+1
+
 
     elif items[i]== '2':
         symbols.pop(len(symbols)-1)
@@ -31,6 +32,34 @@ for i in range(len(length_vector)-1):
         max_length_pos = length_vector[i]+1
 
 
-print(str(max_depth)+" "+str(max_depth_pos)+" "+str(max_length_pos)+" "+str(max_length))
+print(max_depth,max_depth_pos,max_length,max_length_pos)
+
+
+# N = int(input())
+# braces = list(map(int, input().split()))
+# nesting_depth, nesting_depth_index, max_elements_index, max_elements, stack_cnt = 0, 0, 0, 0, 0
+# stack = []
+#
+# for i in range(N):
+#     if braces[i] == 1:
+#         stack.append(i)
+#         stack_cnt += 1
+#         if stack_cnt > nesting_depth:
+#             nesting_depth = stack_cnt
+#             nesting_depth_index = i + 1
+#     elif braces[i] == 2:
+#         stack_cnt -= 1
+#         starting_index = stack.pop()
+#         if len(stack) == 0:
+#             if i - starting_index > max_elements:
+#                 max_elements = i + 1 - starting_index
+#                 max_elements_index = starting_index + 1
+#
+# print(nesting_depth, nesting_depth_index, max_elements, max_elements_index)
+
+# Test cases
+# 14
+# 1 1 2 1 2 1 2 2 1 1 1 2 2 2
+# 3 11 8 1
 
 
