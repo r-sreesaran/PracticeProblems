f074d27944fce22f74e2c59c08f49962c3c12211
r-sreesaran <r.sreesaran@gmail.com>
2020-10-31 09:37:33 +0530

    added solution

--- Solutions/codechef/DSALearningSeries/LinearDataStructures/CHFQUEUE.py
+++ Solutions/codechef/DSALearningSeries/LinearDataStructures/CHFQUEUE.py
@@ -1,2 +1,11 @@
 # https://www.codechef.com/LRNDSA02/problems/CHFQUEUE
-
+n,k = map(int,input().split(" "))
+a = list(input().split(" "))
+product =1
+for i in range(n):
+    for j in range(i+1,n):
+        if(a[i]>a[j]):
+          product *= (j-i+1)
+          product %= 1000000007
+          break
+print(product)
