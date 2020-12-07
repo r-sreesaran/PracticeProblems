ce7c89b325fd4a8b453438ebc8276a7cd20da570
r-sreesaran <r.sreesaran@gmail.com>
2020-12-08 04:18:22 +0530

    added coverage

--- Solutions/codechef/DSALearningSeries/LinearDataStructures/INPSTFIX.py
+++ Solutions/codechef/DSALearningSeries/LinearDataStructures/INPSTFIX.py
@@ -1,27 +1,59 @@
-precedence = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}
-t = int(input())
-for _ in range(t):
+character = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+precedence = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3,'(':0}
+
+def infix_to_postfix(t:int,inexpression,n:int):
+
+    for _ in range(t):
+
+        infix_expression = inexpression
+        stack = []
+        str = ''
+        expression = []
+        for operand in infix_expression:
+            if operand in character:
+                str += operand
+
+            elif operand!='(' and operand!=')':
+
+                if precedence.get(operand) >= evaluvator(stack):
+                    stack.append(operand)
+                else:
+                    while True:
+                        str += stack.pop()
+                        if (len(stack) == 0):
+                            break
+                        if stack[-1] != '(' :
+                            break
+                        if precedence.get(operand) >= precedence.get(stack[-1]):
+                            break
+                    stack.append(operand)
+            if operand == '(':
+                stack.append('(')
+            if operand == ')':
+                while len(stack) > 0 and stack[-1] != '(':
+                    str += stack.pop()
+                stack.pop()
+
+
+        while (len(stack) > 0):
+            str += stack.pop()
+        print(str)
+        return str
+
+def evaluvator(stack) :
+     if len(stack) > 0:
+        return precedence.get(stack[-1])
+     else:
+        return 0
+
+
+if __name__ == "__main__":
+    t = int(input())
+    inexpression = list(input())
     n = int(input())
-    infix_expression = list(input())
-    stack = []
-    str = ''
-    expression = []
-    for operand in infix_expression:
-        if operand == '(':
-            stack.append('(')
-        elif operand in precedence.keys():
-            while len(stack) > 0 and stack[-1] != '(' and precedence.get(operand) <= precedence.get(stack[-1]):
-                str+=stack.pop()
-            stack.append(operand)
-        elif operand == ')':
-            while len(stack) > 0 and stack[-1] != '(':
-                str+=stack.pop()
-            stack.pop()
-        else:
-            str+=(operand)
+    try:
+       infix_to_postfix(t, inexpression, n)
+    except Exception:
+        pass
 
-    while (len(stack) > 0):
-        str+=stack.pop()
-
-
-    print(str)
---
+++ Solutions/codechef/DSALearningSeries/LinearDataStructures/INPSTFIXTest.py
@@ -0,0 +1,30 @@
+import unittest
+from INPSTFIX  import  infix_to_postfix
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(infix_to_postfix(1,"((A+B)*D)^(E-F)",15), "AB+D*EF-^")
+
+    def test_something1(self):
+        self.assertEqual(infix_to_postfix(1,"A+(B*C-(D/E^F)*G)*H",19), "ABC*DEF^/G*-H*+")
+
+    def test_something2(self):
+        self.assertEqual(infix_to_postfix(1,"(A+B)",5), "AB+")
+
+    def test_something3(self):
+        self.assertEqual(infix_to_postfix(1,"A+B+C",5), "ABC++")
+
+    def test_something4(self):
+        self.assertEqual(infix_to_postfix(1, "A*B+C", 5), "AB*C+")
+
+    def test_something5(self):
+        self.assertEqual(infix_to_postfix(1, "(A+B)*C", 5), "AB+C*")
+
+
+    def test_something6(self):
+        self.assertEqual(infix_to_postfix(1, "(A+B/(D^E))*C", 11), "AB+DE^/C*")
+
+
+if __name__ == '__main__':
+    unittest.main()
