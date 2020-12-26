f4914b1a6751a78e9ceb0a725f1f9d26ff47482e
r-sreesaran <r.sreesaran@gmail.com>
2020-12-26 17:00:01 +0530

    added correct solution

--- Solutions/codechef/DSALearningSeries/LinearDataStructures/wormwholes.py
+++ Solutions/codechef/DSALearningSeries/LinearDataStructures/wormwholes.py
@@ -1,45 +1,49 @@
 #https://www.codechef.com/LRNDSA02/problems/ZCO12002
 
-n,x,y = map(int,input().split(" "))
-contest = []
 #Binary search
-def find_nearpoint(time,wormwholes,len_wormwholes,start_wormhole=False):
+def find_nearpoint(time,wormwholes,len_wormwholes,start_wormhole):
     start = 0
     end = len_wormwholes-1
 
-    while start<end:
+    while start<=end:
       mid = start + int((end - start) / 2)
       if(time==wormwholes[mid]):
         return wormwholes[mid]
-      elif(time<wormwholes[mid]):
-        end = mid-1
+      elif(wormwholes[mid]<time):
+        start  = mid+1
       else :
-        start = mid+1
+        end = mid-1
 
     if start_wormhole:
-        return wormwholes[mid]
+        return wormwholes[mid] if time> wormwholes[mid] else wormwholes[mid-1]
     else:
-        return wormwholes[mid+1]
+        return wormwholes[mid] if time< wormwholes[mid] else wormwholes[mid+1]
 
-for _ in range(n):
-    s,e = map(int,input().split(" "))
-    contest.append([s,e])
-v = list(map(int,input().split(" ")))
-w = list(map(int,input().split(" ")))
+def find_mintime(v,w,x,y,contest):
+    v.sort();
+    w.sort();
+    min_time = 1000000;
+    for i in range(n):
+        s, e = contest[i][0], contest[i][1]
+        if (s>=v[0] and e<=w[-1]):
 
-v.sort();
-w.sort();
-min_time = 1000000;
+         start_time = find_nearpoint(s, v, x,True);
+         end_time = find_nearpoint(e, w, y,False);
+         time = end_time - start_time + 1
+         if (time < min_time and time > 0):
+            min_time = time
+
+    print(min_time)
+
 
 # Naive implementation o(n^2)
 # for i in range(n):
 #     s,e = contest[i][0],contest[i][1]
-#     start_time = 0;
+#     start_time = 10000;
 #     end_time = 0;
 #     for j in range(x):
 #         if (v[j]<=s):
 #            start_time = v[j]
-#         else:
 #            break;
 #     for j in range(y):
 #         if(w[j]>=e):
@@ -49,12 +53,13 @@ min_time = 1000000;
 #     if(time<min_time and time > 0):
 #         min_time = time
 
-for i in range(n):
-    s,e = contest[i][0],contest[i][1]
-    start_time = find_nearpoint(s,v,x,True);
-    end_time = find_nearpoint(s,w,y);
-    time = end_time - start_time +1
-    if(time<min_time and time > 0):
-        min_time = time
 
-print(min_time)
+if __name__ == "__main__":
+    n, x, y = map(int, input().split(" "))
+    contest = []
+    for _ in range(n):
+        s, e = map(int, input().split(" "))
+        contest.append([s, e])
+    v = list(map(int, input().split(" ")))
+    w = list(map(int, input().split(" ")))
+    find_mintime(v,w,x,y,contest)
