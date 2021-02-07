32092a04d8666649584d5175abeb242804078239
r-sreesaran <r.sreesaran@gmail.com>
2021-02-07 17:54:06 +0530

    started with the problem

--- Solutions/codechef/DSALearningSeries/LinearDataStructures/rectangle.py
+++ Solutions/codechef/DSALearningSeries/LinearDataStructures/rectangle.py
@@ -1,1 +1,15 @@
 #https://www.codechef.com/LRNDSA02/problems/ZCO15004
+
+no_of_points  = int(input())
+
+points = []
+for _ in range(no_of_points):
+    (x,y) = map(int,input().split(" "))
+    points.append((x,y))
+
+
+# Adding arbitary points
+for _ in range(500):
+    points.append((_,500))
+
+points.append((0,100000))
