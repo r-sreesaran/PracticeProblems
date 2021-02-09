16a473eb857208815ed2e026c75e4fa75481f690
r-sreesaran <r.sreesaran@gmail.com>
2021-02-09 22:32:30 +0530

    added partial solution

--- Solutions/codechef/DSALearningSeries/LinearDataStructures/rectangle.py
+++ Solutions/codechef/DSALearningSeries/LinearDataStructures/rectangle.py
@@ -13,3 +13,44 @@ for _ in range(500):
     points.append((_,500))
 
 points.append((0,100000))
+
+points = sorted(points,key=1)
+points_with_pos = []
+pos=0
+for point in points:
+   points_with_pos.append((point[0],point[1],pos))
+   pos=+1
+
+points = points_with_pos
+rightside_points_finder = [points(0)]
+rightside_points = len(points)*[0]
+position =0
+
+# The tuple will have three points namely the x,y co-cordinate, the position
+
+for point in points:
+
+
+    starting_point = point[0]
+    # comparing the y position
+    if rightside_points_finder[-1][1] > starting_point[1]:
+       current_point = rightside_points.pop()
+       # setting the max point till rectangle can be drawn
+       rightside_points[current_point[3]]=starting_point[0]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
